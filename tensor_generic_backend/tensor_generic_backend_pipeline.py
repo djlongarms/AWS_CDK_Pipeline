1a3d019d38e3b83325d56af9a031a3e6e682fd7e
Nick Palmer <nick.palmer@tensoriot.com>
2022-06-23 17:22:25 -0700

    pipeline code created

--- tensor_generic_backend/tensor_generic_backend_pipeline.py
+++ tensor_generic_backend/tensor_generic_backend_pipeline.py
@@ -1,7 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
-    aws_codecommit as codecommit
+    aws_codecommit as codecommit,
+    pipelines as pipelines
 )
 
 class TensorGenericBackendPipelineStack(Stack):
@@ -11,4 +12,17 @@ class TensorGenericBackendPipelineStack(Stack):
         repo = codecommit.Repository(
             self, "GenericBackendRepo",
             repository_name="GenericBackendRepo"
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self, "GenericPipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.code_commit(repo, "main"),
+                commands=[
+                    "npm install -g aws-cdk",
+                    "pip install -r requirements.txt",
+                    "cdk cynth"
+                ]
+            )
         )
