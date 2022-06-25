dfe7adf76333e7dd3a1adae72e7653640b50cbb6
Nick Palmer <nick.palmer@tensoriot.com>
2022-06-24 18:01:28 -0700

    added parameters for account id and region

--- app.py
+++ app.py
@@ -2,8 +2,17 @@
 import aws_cdk as cdk
 from tensor_generic_backend.tensor_generic_backend_pipeline import TensorGenericBackendPipelineStack
 
-
 app = cdk.App()
-TensorGenericBackendPipelineStack(app, "TensorGenericBackendStack")
+
+account_id_parameter = cdk.CfnParameter(app, "accountID", type="String",
+    description="The account number where the stack will be deployed.")
+
+region_parameter = cdk.CfnParameter(app, "region", type="String",
+    description="The region where the stack will be deployed.")
+
+TensorGenericBackendPipelineStack(
+    app, "TensorGenericBackendStack",
+    env=cdk.Environment(account=account_id, region=region)
+)
 
 app.synth()
