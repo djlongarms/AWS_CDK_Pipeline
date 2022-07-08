4fc8d70b058ac4a5bfd52abc146a2e318d34fa18
Nick Palmer <nick.palmer@tensoriot.com>
2022-07-08 11:34:31 -0700

    added basic documentation for config file

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import aws_cdk as cdk
 from tensor_generic_backend.tensor_generic_backend_pipeline import TensorGenericBackendPipelineStack
 from omegaconf import OmegaConf
 
-conf = OmegaConf.load("config/config.yaml")
-
 app = cdk.App()
 
+conf = OmegaConf.load("config/config.yaml")
 branch = app.node.try_get_context("branch")
 
 TensorGenericBackendPipelineStack(
