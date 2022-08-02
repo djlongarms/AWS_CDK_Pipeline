4059faa08981262df89f70e69b88a901538d6523
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-02 14:52:42 -0700

    moved pylint command to after requirements install

--- stacks/pipeline_stack.py
+++ stacks/pipeline_stack.py
@@ -80,9 +80,9 @@ class TensorGenericBackendPipelineStack(Stack):
                     "BRANCH": branch
                 },
                 commands=[
-                    "pylint --rcfile=./.pylintrc `pwd`/source || pylint-exit -wfail -efail -cfail $?",
                     "npm install -g aws-cdk",
                     "pip install -r requirements.txt",
+                    "pylint --rcfile=./.pylintrc `pwd`/source || pylint-exit -wfail -efail -cfail $?",
                     "cdk synth -c branch=$BRANCH"
                 ]
             )
