f4c912028066637aa813fdf8f19519b46726591d
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-03 11:44:37 -0700

    updated .build.sh file

--- .build.sh
+++ .build.sh
@@ -1,4 +1,4 @@
 npm install -g aws-cdk
 pip install -r requirements.txt
-# pylint --rcfile=./.pylintrc/pylintrc `pwd`/source || pylint-exit -wfail -efail -cfail $?
+pylint --rcfile=./.pylintrc `pwd` || pylint-exit -wfail -efail -cfail $?
 cdk synth -c branch=$BRANCH
