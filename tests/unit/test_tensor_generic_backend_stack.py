e327b0316f4ad4859dcda0182090b721d3307022
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-04 12:41:53 -0700

    reupdated tests file to pylint standards

--- tests/unit/test_tensor_generic_backend_stack.py
+++ tests/unit/test_tensor_generic_backend_stack.py
@@ -1,7 +1,6 @@
 """This is meant as a place to create tests for your stacks
 to run against if so desired."""
 import aws_cdk as core
-import aws_cdk.assertions as assertions
 
 from stacks.resource_stack import TensorGenericBackendStack
 
@@ -11,7 +10,7 @@ def test_sqs_queue_created(conf):
   """Generic test method created when cdk app is initialized."""
   app = core.App()
   stack = TensorGenericBackendStack(app, "tensor-generic-backend", conf=conf)
-  assertions.Template.from_stack(stack)
+  core.assertions.Template.from_stack(stack)
 
 #   template.has_resource_properties("AWS::SQS::Queue", {
 #     "VisibilityTimeout": 300
