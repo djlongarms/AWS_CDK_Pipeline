2153c24d8fcb999368c4604eed2430eb681b8704
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-04 12:23:10 -0700

    updated app.py to pylint standards

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+"""This file is run when deploying stacks to CloudFormation.
+This file should not be changed unless absolutely necessary."""
 #!/usr/bin/env python3
-import aws_cdk as cdk
 import json
+import aws_cdk as cdk
 
 from stacks.pipeline_stack import TensorGenericBackendPipelineStack
 
@@ -8,18 +10,19 @@ from stacks.pipeline_stack import TensorGenericBackendPipelineStack
 app = cdk.App()
 
 # Retrieves config file and branch context variable
-conf = json.load(open("config/config.json"))
+with open("config/config.json", encoding='UTF-8') as conf_file:
+  conf = json.load(conf_file)
 branch = app.node.try_get_context("branch")
 
-# Initializes pipeline 
+# Initializes pipeline
 TensorGenericBackendPipelineStack(
-    app, f"{conf['resource_ids']['pipeline_stack_id']}-{branch}",
-    env = cdk.Environment(
-        account=conf['aws']['account'],
-        region=conf['aws']['region']
-    ),
-    conf=conf,
-    branch=branch
+  app, f"{conf['resource_ids']['pipeline_stack_id']}-{branch}",
+  env = cdk.Environment(
+    account=conf['aws']['account'],
+    region=conf['aws']['region']
+  ),
+  conf=conf,
+  branch=branch
 )
 
 # Synthesizes application for cloud deployment
