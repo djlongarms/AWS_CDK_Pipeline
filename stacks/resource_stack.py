3f536d16c1d8b37c350de7a807b6ccd7d3ff5e32
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-04 14:39:00 -0700

    updated resource stack file to pylint standards

--- stacks/resource_stack.py
+++ stacks/resource_stack.py
@@ -1,8 +1,13 @@
+"""This file is where resources will be placed to be created in the stack.
+The pipeline will create a deployment for each stage and deploy resources
+as described here. A user can create resource files in the 'resources' folder,
+import them here, and instantiate them in the stack. Then, once deployed,
+the resources will be created in the AWS account associated with the stage."""
 import sys
 import os
 from constructs import Construct
 from aws_cdk import (
-    Stack
+  Stack
 )
 
 # Append resources folder to path for importing resources.
@@ -14,7 +19,9 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 # Resource stack for deployment through pipeline
 class TensorGenericBackendStack(Stack):
+  """This is the resource stack for the resources to be deployed."""
+  def __init__(self, scope: Construct, construct_id: str, conf, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-    def __init__(self, scope: Construct, construct_id: str, conf, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-        
+    print(conf)
+    
