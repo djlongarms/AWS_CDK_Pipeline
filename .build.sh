1ca2cadd87da452a4f0bb07aaa7e530f85975ae0
Nick Palmer <nick.palmer@tensoriot.com>
2022-08-05 11:58:42 -0700

    modified build file to fail on pylint errors

--- .build.sh
+++ .build.sh
@@ -1,4 +1,7 @@
 npm install -g aws-cdk
 pip install -r requirements.txt
 pylint --rcfile=./.pylintrc `pwd` || pylint-exit -wfail -efail -cfail $?
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 cdk synth -c branch=$BRANCH
